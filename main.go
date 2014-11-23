db122602f842f54c0be3a914e6d231f090458a43
David Long <longda@gmail.com>
2014-11-23 12:19:37 -0800

    Broken checkpoint.

--- main.go
+++ main.go
@@ -4,59 +4,36 @@ import (
   "fmt"
   "io"
   "log"
-  "net"
+  "net/http"
+
+  "code.google.com/p/go.net/websocket"
 )
 
 const LISTEN_ADDR = "localhost:4000"
 
-var partner = make(chan io.ReadWriteCloser)
-
 func main() {
-  l, err := net.Listen("tcp", LISTEN_ADDR)
-  
+  http.HandleFunc("/", rootHandler)
+  http.Handle("/socket", websocket.Handler(socketHandler))
+
+  err := http.ListenAndServe(LISTEN_ADDR, nil)
+
   if err != nil {
     log.Fatal(err)
   }
-
-  for {
-    c, err := l.Accept()
-    
-    if err != nil {
-      log.Fatal(err)
-    }
-
-    go match(c)
-  }
 }
 
-func match(c io.ReadWriteCloser) {
-  fmt.Fprint(c, "Waiting for a partner...")
+import "html/template"
 
-  select {
-  case partner <- c:
-    // now handled by the other goroutine
-  case p := <- partner:
-    chat(p, c)
-  }
+func rootHandler(w http.ResponseWriter, *http.Request) {
+  rootTemplate.Execute(w, listenAddr)
 }
 
-func chat(a, b io.ReadWriteCloser) {
-  fmt.Fprintln(a, "Found one! Say hi.")
-  fmt.Fprintln(b, "Found one! Say hi.")
-
-  errc := make(chan error, 1)
-  go cp(a, b, errc)
-  go cp(b, a, errc)
-
-  if err := <- errc; err != nil {
-    log.Println(err)
-  }
-
-  a.Close()
-  b.Close()
-}
-
-func cp(w io.Writer, r io.Reader, errc chan<- error) {
-  _, err := io.Copy(w, r)
-  errc <- err
-}
+var rootTemplate = template.Must(template.New("root").Parse(`
+<!DOCTYPE html>
+<htmll>
+<head>
+<meta charset="utf-8" />
+<script>
+</script>
+</html>
+  `))
