713286035d9ee411ac6e1c20dd1acfaed73e9127
David Long <longda@gmail.com>
2014-11-23 15:16:12 -0800

    Finished up to slide 32.

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
   "net/http"
 
   "code.google.com/p/go.net/websocket"
+
+  "html/template"
 )
 
 const LISTEN_ADDR = "localhost:4000"
@@ -22,10 +24,8 @@ func main() {
   }
 }
 
-import "html/template"
-
-func rootHandler(w http.ResponseWriter, *http.Request) {
-  rootTemplate.Execute(w, listenAddr)
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+  rootTemplate.Execute(w, LISTEN_ADDR)
 }
 
 var rootTemplate = template.Must(template.New("root").Parse(`
@@ -37,3 +37,57 @@ var rootTemplate = template.Must(template.New("root").Parse(`
 </script>
 </html>
   `))
+
+type socket struct {
+  conn *websocket.Conn
+  done chan bool
+}
+
+func (s socket) Read(b []byte) (int, error) { return s.conn.Read(b) }
+func (s socket) Write(b []byte) (int, error) { return s.conn.Write(b) }
+
+func (s socket) Close() error {
+  s.done <- true
+  return nil
+}
+
+func socketHandler(ws * websocket.Conn) {
+  s := socket{conn: ws, done: make(chan bool)}
+  go match(s)
+  <- s.done
+}
+
+var partner = make(chan io.ReadWriteCloser)
+
+func match(c io.ReadWriteCloser) {
+    fmt.Fprint(c, "Waiting for a partner...")
+    
+    select {
+    case partner <- c:
+        // now handled by the other goroutine
+    case p := <-partner:
+        chat(p, c)
+    }
+}
+
+func chat(a, b io.ReadWriteCloser) {
+    fmt.Fprintln(a, "Found one! Say hi.")
+    fmt.Fprintln(b, "Found one! Say hi.")
+    
+    errc := make(chan error, 1)
+    
+    go cp(a, b, errc)
+    go cp(b, a, errc)
+    
+    if err := <-errc; err != nil {
+        log.Println(err)
+    }
+    
+    a.Close()
+    b.Close()
+}
+
+func cp(w io.Writer, r io.Reader, errc chan<- error) {
+    _, err := io.Copy(w, r)
+    errc <- err
+}
