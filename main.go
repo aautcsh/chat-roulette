e5ed71b9b519202f76b13ee44e931542d4374401
David Long <longda@gmail.com>
2014-11-22 17:26:39 -0800

    Added error handling.

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ func main() {
 
 func match(c io.ReadWriteCloser) {
   fmt.Fprint(c, "Waiting for a partner...")
+
   select {
   case partner <- c:
     // now handled by the other goroutine
@@ -42,6 +43,20 @@ func match(c io.ReadWriteCloser) {
 func chat(a, b io.ReadWriteCloser) {
   fmt.Fprintln(a, "Found one! Say hi.")
   fmt.Fprintln(b, "Found one! Say hi.")
-  go io.Copy(a, b)
-  io.Copy(b, a)
+
+  errc := make(chan error, 1)
+  go cp(a, b, errc)
+  go cp(b, a, errc)
+
+  if err := <- errc; err != nil {
+    log.Println(err)
+  }
+
+  a.Close()
+  b.Close()
+}
+
+func cp(w io.Writer, r io.Reader, errc chan<- error) {
+  _, err := io.Copy(w, r)
+  errc <- err
 }
