65f7e5ac07382df2e493c109810922a9ac32bb9f
David Long <longda@gmail.com>
2014-11-23 16:45:14 -0800

    Web sockets working!!!

--- main.go
+++ main.go
@@ -29,22 +29,72 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 var rootTemplate = template.Must(template.New("root").Parse(`
+<!-- FROM: https://www.websocket.org/echo.html -->
 <!DOCTYPE html>
 <htmll>
 <head>
 <meta charset="utf-8" />
-<script>
+<script language="javascript" type="text/javascript">
+//var wsUri = "ws://echo.websocket.org/"; 
+var wsUri = "ws://{{.}}/socket"; 
+var output;
+var input;
+var send;
+function init() {
+  output = document.getElementById("output");
+  input = document.getElementById("input");
+  send = document.getElementById("send");
+  send.onclick = sendClickHandler;
+  input.onkeydown = function(event) { if (event.keyCode == 13) send.click(); };
+  testWebSocket();
+}
+function sendClickHandler() {
+  doSend(input.value);
+  input.value = '';
+}
+function testWebSocket() {
+  websocket = new WebSocket(wsUri);
+  websocket.onopen = function(evt) { onOpen(evt) };
+  websocket.onclose = function(evt) { onClose(evt) };
+  websocket.onmessage = function(evt) { onMessage(evt) };
+  websocket.onerror = function(evt) { onError(evt) }; 
+}
+function onOpen(evt) {
+  writeToScreen("CONNECTED");
+  //doSend("WebSocket rocks");
+}
+function onClose(evt) {
+  writeToScreen("DISCONNECTED");
+}
+function onMessage(evt) {
+  writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
+  //websocket.close();
+}
+function onError(evt) {
+  writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data); 
+}
+function doSend(message) {
+  writeToScreen("SENT: " + message);
+  websocket.send(message);
+}
+function writeToScreen(message) {
+  var pre = document.createElement("p");
+  pre.style.wordWrap = "break-word";
+  pre.innerHTML = message;
+  output.appendChild(pre);
+}
+window.addEventListener("load", init, false);
 </script>
+<h2>WebSocket Test</h2>
+<input type="text" id="input" /><input type="button" id="send" value="Send" />
+<div id="output"></div>
 </html>
   `))
 
 type socket struct {
-  conn *websocket.Conn
+  io.ReadWriter
   done chan bool
 }
-
-func (s socket) Read(b []byte) (int, error) { return s.conn.Read(b) }
-func (s socket) Write(b []byte) (int, error) { return s.conn.Write(b) }
 
 func (s socket) Close() error {
   s.done <- true
@@ -52,7 +102,7 @@ func (s socket) Close() error {
 }
 
 func socketHandler(ws * websocket.Conn) {
-  s := socket{conn: ws, done: make(chan bool)}
+  s := socket{ws, make(chan bool)}
   go match(s)
   <- s.done
 }
